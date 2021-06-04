0b0d7c184f6d88548365e98a31d775ddef1aa775
Marcello Chiesa <marci6@users.noreply.github.com>
2021-06-04 16:25:43 +0200

    Added CNN

--- Code/param.py
+++ Code/param.py
@@ -18,7 +18,7 @@ class Parameters:
         self.std2 = 6.0
         self.pi = 0.25
         self.seed = 0
-        self.experiment = 'mnist5'
+        self.experiment = 'cifar'
         self.approach = 'ucb'
         self.inputsize = None
         self.taskcla = None
